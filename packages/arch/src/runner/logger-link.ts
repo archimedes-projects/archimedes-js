4307dd766b193be0479638d0e6eba30437bd91d5
César Alberca <cesalberca@gmail.com>
2021-05-24 16:26:26 +0200

    fix: add object formatting

--- packages/arch/src/runner/logger-link.ts
+++ packages/arch/src/runner/logger-link.ts
@@ -12,11 +12,15 @@ export class LoggerLink extends BaseLink {
     context.result = context.result?.then(x => {
       this.logger.log(
         `[${Datetime.now().toIso()}] ${context.useCase.constructor.name} / Params: ${
-          context.param !== undefined ? JSON.stringify(context.param, null, 2) : ''
-        } - Result: ${x}`
+          context.param !== undefined ? this.formatObject(context.param) : ''
+        } - Result: ${this.formatObject(x)}`
       )
       return x
     })
     this.nextLink.next(context)
+  }
+
+  private formatObject(object: unknown) {
+    return JSON.stringify(object, null, 2)
   }
 }
