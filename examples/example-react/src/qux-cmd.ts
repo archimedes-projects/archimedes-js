e5cd0ea66cda077bb65ce69676df380d5b568138
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-06-08 15:45:07 +0200

    feat: remove old React example

--- examples/example-react/src/qux-cmd.ts
+++
@@ -1,8 +0,0 @@
-import { injectable } from 'tsyringe'
-import { Command, EvictCache } from '@archimedes/arch'
-
-@EvictCache
-@injectable()
-export class QuxCmd extends Command<number> {
-  async internalExecute(value: number) {}
-}
--- examples/example-react/src/use-di.ts
+++
@@ -1,6 +0,0 @@
-import { container } from './container'
-import { constructor } from 'tsyringe/dist/typings/types'
-
-export function useDi<T>(token: constructor<T> | string) {
-  return container.resolve(token)
-}
